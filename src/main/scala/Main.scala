c907656e2e556420cd87f50498bcac692e6a8c51
Bryan Hunt <bryan@zeebox.com>
2013-07-19 18:25:32 +0100

    Finished MVP.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,19 +2,29 @@ import moon.Spreadsheet
 
 object Main {
   def main(args: Array[String]) {
-
-
     val s = new Spreadsheet()
-    s.assign("A1", "2")
-    s.assign("A2", "foobar")
-    s.assign("A2", "1")
-    s.assign("A3", "1")
-    s.assign("A4", "1")
-    s.assign("A5", "=MAX(A1:A4)")
-    s.assign("B1", "2")
-    s.assign("C1", "2")
-    s.assign("E7", "SUM COSTS")
-    s.assign("F7", "=SUM(A1:A5)")
+
+    val cmds = List(
+      ("A1" -> "2"),
+      ("A2" -> "foobar"),
+      ("A2" -> "1"),
+      ("A3" -> "foobar"),
+      ("A4" -> "1"),
+      ("A5" -> "=MAX(A1:A4)"),
+      ("B1" -> "2"),
+      ("C1" -> "2"),
+      ("E7" -> "SUM COSTS"),
+      ("F7" -> "=SUM(A1:A5)")
+    )
+
+    println("Executing cmds:")
+    cmds.foreach {
+      c =>
+        println(c)
+        s.assign(c._1, c._2)
+    }
+
+
     s.render.foreach(println)
   }
 }
